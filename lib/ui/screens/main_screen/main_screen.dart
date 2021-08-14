c6f2dc1ca3660b37da19d33c6a194edd84d111a9
Dani Roman Martinez <droman@leadtech.com>
2021-08-14 13:14:52 +0200

    Reformat code

--- lib/ui/screens/main_screen/main_screen.dart
+++ lib/ui/screens/main_screen/main_screen.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:my_fit_evolution/ui/theme/app_bar.dart';
 
 class MainScreen extends StatelessWidget {
-
   static const ROUTE = '/main';
 
   const MainScreen();
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: const MyFitEvolutionAppBar(),
       body: Text('Main Screen'),
